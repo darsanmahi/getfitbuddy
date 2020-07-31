1ed9567c31cd6c4002816adf47fa5814a027e53e
developers-headerfile <52238291+developers-headerfile@users.noreply.github.com>
2020-07-31 15:46:46 +0530

    Completion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { AuthenticationService } from './services/authservice.service';
 import { Component, OnInit } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
@@ -5,28 +7,30 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
 export class AppComponent implements OnInit {
   public selectedIndex = 0;
   public appPages = [
     {
-      title: 'Home',
-      url: '/home',
-      icon: 'home'
+      title: "Home",
+      url: "/home",
+      icon: "home",
     },
     {
-      title: 'Workout',
-      url: '/workout',
-      icon: 'barbell'
+      title: "Workout",
+      url: "/workout",
+      icon: "barbell",
     },
   ];
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private authservice: AuthenticationService,
+    private router: Router
   ) {
     this.initializeApp();
   }
@@ -39,9 +43,16 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    const path = window.location.pathname.split('folder/')[1];
+    const path = window.location.pathname.split("folder/")[1];
     if (path !== undefined) {
-      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
+      this.selectedIndex = this.appPages.findIndex(
+        (page) => page.title.toLowerCase() === path.toLowerCase()
+      );
     }
+  }
+  signOut(){
+    this.authservice.SignOut().then((a) =>
+      this.router.navigate(['/login'])
+    );
   }
 }
